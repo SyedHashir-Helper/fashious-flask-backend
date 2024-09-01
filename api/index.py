baea712d47264702dc03f675c65b674be75a31b0
Syed Hashir Husnain <98015136+SyedHashir-Helper@users.noreply.github.com>
2024-09-01 19:20:45 +0500

    Update index.py

--- api/index.py
+++ api/index.py
@@ -8,7 +8,7 @@ import os
 app = Flask(__name__)
 
 CORS(app, resources={
-    r"/api/*": {
+    r"/*": {
         "origins": ["http://localhost:5173", "http://127.0.0.1:5173", "fashious-genai-hashir.netlify.app", "https://fashious-genai-hashir.netlify.app/", "https://fashious-genai-hashir.netlify.app"],
         "methods": ["GET", "POST"]
     }
